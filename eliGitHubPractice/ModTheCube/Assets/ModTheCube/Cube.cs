ac183864d4bad4ead39c37a6d21bff340c0706c6
Eli Watts <42781660+wattse13@users.noreply.github.com>
2021-02-02 20:29:55 +0100

    GitHub Collab Practice
    I am uploading these files to practice using GitHub Collaboratively.
    I've included a sample project, a readme.md, and a .gitignore file.

---
+++ eliGitHubPractice/ModTheCube/Assets/ModTheCube/Cube.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    public MeshRenderer Renderer;
+    
+    //Rotate Variables
+    public GameObject target;
+    public float spinSpeed = 20.0f;
+    public float rotateSpeed = 10.0f * Time.deltaTime;
+
+    //Color Variables
+    public float colorDelay = 2.0f;
+
+    //Scale Variables
+    public float growDelay = 2.0f;
+    public float originalSize = 1.3f;
+
+    void Start()
+    {
+        transform.position = new Vector3(3, 4, 1);
+
+        InvokeRepeating("GoChamaleon", colorDelay, colorDelay);
+
+        InvokeRepeating("GrowingShrinking", growDelay, growDelay);
+    }
+    
+    void Update()
+    {
+        transform.Rotate(rotateSpeed * Time.deltaTime, rotateSpeed, rotateSpeed);
+
+        //Rotates cube around rotatePoint game object
+        transform.RotateAround(target.transform.position, Vector3.up, spinSpeed * Time.deltaTime);
+    }
+    void GoChamaleon()
+    {
+        Material material = Renderer.material;
+        material.color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+    }
+
+    void GrowingShrinking()
+    {
+        originalSize = Random.Range(1, 5);
+        transform.localScale = Vector3.one * originalSize;
+    }
+}
---
+++ eliGitHubPractice/readme.md.txt
@@ -0,0 +1,15 @@
+# GitHub Collaboration Practice
+
+## This project was made in Unity version 2018.4.30f1
+
+I added this project to the repository so that I could practice using GitHub collaboratively. I hope I am doing this correctly :)
+
+After hitting the play button, the cube should rotate around a central axis. Occaisionally it should change size and color.
+
+I found this tutorial very helpful: [How to useGitHub with Unity](https://www.youtube.com/watch?v=qpXxcvS-g3g&t=337s).
+
+Please feel free to fork and change and break and whatever else yall want to do! 
+
+## GitIgnore
+
+While following the tutorial, I came across the subject of a .gitignore file which massively reduces the number of files being commited. Seriously, in this little project I went from over 3,000 files to 34 by adding the .gitignore file. That said, I don't actually know what I am doing. Maybe I am excluding important files. Maybe I could exclude more files. Maybe I should be doing something else entirely. Based on some very preliminary research, it looks like the later versions of GitHub have some different standards for using .gitignore files and there is even a github add on made specifically for unity. 
